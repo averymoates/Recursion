64ffa9362f8f49c0a855a6225af217ae3fe1124f
Avery <avery.moates17@ou.edu>
2020-11-09 16:39:42 -0600

    Wrote the eating of cake methods.

--- src/Cake.java
+++ src/Cake.java
@@ -1,4 +1,21 @@
 
 public class Cake {
-
+	
+	private int bites = 10;
+	
+	public Cake() {
+		//Nothing to implement 
+	}
+	
+	public void takeABite() {
+		--this.bites;
+		System.out.println("Ehh, could be better.");
+	}
+	
+	public boolean isEmpty() {
+		return bites==0;
+		//Looks like an empty cake.
+	}
+	
+	
 }
